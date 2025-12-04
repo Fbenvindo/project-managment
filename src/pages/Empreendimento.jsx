3bff539e3ccccbe59aed1e74d56f5a7f16d72ce2
Fabricio Benvindo <fbenvindo77@gmail.com>
2025-12-04 13:31:55 +0000

    File changes

--- src/pages/Empreendimento.jsx
+++ src/pages/Empreendimento.jsx
@@ -324,7 +324,7 @@ export default function EmpreendimentoPage() {
         <EmpreendimentoHeader empreendimento={empreendimento} />
 
         <Tabs value={activeTab} onValueChange={handleTabChange} className="w-full">
-          <TabsList className={`grid w-full ${hasAccessToGestao ? 'grid-cols-2 sm:grid-cols-3 lg:grid-cols-6' : 'grid-cols-2 sm:grid-cols-3 lg:grid-cols-5'} bg-white shadow-sm`}>
+          <TabsList className={`grid w-full ${hasAccessToGestao ? 'grid-cols-2 sm:grid-cols-4 lg:grid-cols-7' : 'grid-cols-2 sm:grid-cols-3 lg:grid-cols-6'} bg-white shadow-sm`}>
             <TabsTrigger value="documentos">Documentos</TabsTrigger>
             <TabsTrigger value="pavimentos">Pavimentos</TabsTrigger>
             <TabsTrigger value="atividades_projeto">Atividades do Projeto</TabsTrigger>
@@ -332,6 +332,7 @@ export default function EmpreendimentoPage() {
               <ListChecks className="w-4 h-4 mr-2" /> Catálogo
             </TabsTrigger>
             <TabsTrigger value="documentacao">Documentação</TabsTrigger>
+            <TabsTrigger value="alocacao">Alocação Equipe</TabsTrigger>
             {hasAccessToGestao && (
               <TabsTrigger value="gestao">Gestão</TabsTrigger>
             )}
